04d1734c36c3796ae552ee81e1782d99a0b668d4
R0b0so <ethansagar222@gmail.com>
2025-01-12 17:23:50 -0800

    hello

---
+++ homework.py
@@ -0,0 +1,8 @@
+
+shutdown = input("do you want to shutdown your computer?")
+if shutdown == "no":
+    print("system not shutting down")
+elif shutdown == "yes":
+    print("system shutting down")
+else: 
+    print("sorry we didn't get that") 
